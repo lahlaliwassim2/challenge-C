fa74624b09c4af970c8dc80110b8f3f1468e53a5
lahlaliwassim2 <wlahlali34@gmail.com>
2021-11-12 15:21:27 +0100

    plus9

---
+++ variables/chal9.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+
+	float l , C ,L;
+	printf("ce programme permet de calculer la circonfence d un rectangle \n ");
+	printf("entrer la valeur de largeur :\n");
+	scanf("%f",&l);
+	printf("entrer la valeur de langeur :\n");
+	scanf("%f",&L);
+	C=(L+l)*2;
+	printf("la circonfence C =%f",C);
+	return 0;
+}
+
+
