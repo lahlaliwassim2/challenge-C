529fac396eb634d8c83ee30fd7235584df25bed3
lahlaliwassim2 <wlahlali343@gmail.com>
2021-11-13 21:08:08 +0100

    plus3

---
+++ Conditions/chall 2 2.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	char A ;
+	
+	printf("voyelle ou non \n");
+	
+	printf("saisir une lettre \n");
+	scanf("%c",&A);
+	
+	switch(A)
+	{
+		case 'a' :
+			printf("cette lettre est voyelle");
+			break;
+		case 'e' :
+		printf("cette lettre est voyelle ");
+	      	break;
+			
+		case 'i' :
+		printf("cette lettre est voyelle ");
+	      	break;
+			
+	case 'u' :
+		printf("cette lettre est voyelle ");
+	      	break;
+	      	
+			case 'o' :
+		printf("cette lettre est voyelle ");
+	      	break;
+	      	
+	      	case 'y' :
+		printf("cette lettre est voyelle ");
+	      	break;
+	      	case 'A' :
+			printf("cette lettre est voyelle");
+			break;
+		case 'E' :
+		printf("cette lettre est voyelle ");
+	      	break;
+			
+		case 'I' :
+		printf("cette lettre est voyelle ");
+	      	break;
+			
+	case 'U' :
+		printf("cette lettre est voyelle ");
+	      	break;
+	      	
+			case 'O' :
+		printf("cette lettre est voyelle ");
+	      	break;
+	      	
+	      	case 'Y' :
+		printf("cette lettre est voyelle ");
+	      	break;
+	      	
+	      	default : printf("cette lettre n est pas voyelle ");
+			break ;
+			}	return 0;
+}
