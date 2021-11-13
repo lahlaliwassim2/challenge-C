573d99d2b3872f1df42f41f64ba124a095d2a993
lahlaliwassim2 <wlahlali343@gmail.com>
2021-11-13 21:12:40 +0100

    plus 3

--- Conditions/chall 2 3.c
+++ Conditions/chall 2 3.c
@@ -1,5 +1,4 @@
-#include <iostream>
-#include <unistd.h>
+
 #include<stdio.h>
 #include<stdlib.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
