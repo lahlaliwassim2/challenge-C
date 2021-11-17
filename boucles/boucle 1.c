e42d2b7b8deb66da7cdfffe559f6eadefa2da0c0
lahlaliwassim2 <wlahlali343@gmail.com>
2021-11-17 07:18:50 +0100

    plus boucles 1

---
+++ boucles/boucle 1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	int a,i;
+	printf("taper un nombre entra 1 et 9\n ");
+	scanf("%d",&a);
+	for(i=1;i<10;i++){
+		printf("%d*%d=%d\n",a,i,a*i);
+	}
+	return 0;
+}
